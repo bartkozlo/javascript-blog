69a761bdc3395791ad181e965d6d3c3b3ac6c6d3
Bartosz Kozłowski <bartkozlo@gmail.com>
2021-07-07 21:46:36 +0200

    Get 'href' attribute from the clicked link

--- js/script.js
+++ js/script.js
@@ -35,6 +35,10 @@ const titleClickHandler = function(event){
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
+
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
